1bb78e71b9719c4c88d07a403939bed1bfebeb50
Christopher Mevissen <cmevissen@hostgator.com>
2015-05-18 01:52:37 -0400

    added login page with session validation

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect, abort, session
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name,PerlLexer
 from pygments.formatters import HtmlFormatter
@@ -8,22 +8,37 @@ import yaml
 
 app = Flask('psi')
 app.debug = True
+app.secret_key = 'super secret'
 config = yaml.load(open("config.yaml", "r"))
 
 def get_db():
     con = MySQLdb.connect(host = config['db_host'], db = config['db_name'], user = config['db_user'], passwd = config['db_pass']);
     return con.cursor()
     
-@app.route('/hello')
-def hello():
-    return "hello, world"
+@app.route('/login', methods=['GET'])
+def get_login():
+    return render_template('login.html');
+
+@app.route('/login', methods=['POST'])
+def post_login():
+    if request.form['username'] == 'super' and request.form['password'] == 'secret':
+        session['username'] = request.form['username']
+        return redirect(url_for('home'))
+    else:
+        return 'wrong username or password', 401
 
 @app.route('/')
 def home():
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+
     return render_template('home.html')
 
 @app.route('/search')
 def search():
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+        
     text = request.args.get('text')
     db = get_db();
     db.execute("select p.name from packages p join tags t on t.package_id = p.id where t.name like %s order by p.name", (text + '%',))
@@ -33,6 +48,9 @@ def search():
 
 @app.route('/properties')
 def properties():
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+
     package = request.args.get('package')
     db = get_db()
     db.execute("select p.name, p.type, p.id from properties p join packages pack on pack.id = p.package_id where pack.name = %s and p.name not like '\_%%' order by p.name", (package,))
@@ -50,6 +68,9 @@ def properties():
 
 @app.route("/source")
 def get_source():
+    if 'username' not in session:
+        return redirect(url_for('get_login'))
+
     package = request.args.get('package')
     db = get_db()
     db.execute("select source from packages where name = %s", (package,))
